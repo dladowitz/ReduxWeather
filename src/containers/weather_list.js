d3ca20c9995d559ee09d852a75ef5f60aa43cb3e
David Ladowitz <david@ladowitz.com>
2016-09-30 16:46:21 -0700

    finishing up with maps and middleware" gs gi

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,33 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart'
+import GMap from '../components/gmap'
 
 
 class WeatherList extends Component {
   renderWeather(city){
     const name = city.city.name
     const temps = city.list.map(weatherData => weatherData.main.temp)
-    const humidity = city.list.map(weatherData => weatherData.main.humidity)
-    const pressure = city.list.map(weatherData => weatherData.main.pressure)
+    const humidities = city.list.map(weatherData => weatherData.main.humidity)
+    const pressures = city.list.map(weatherData => weatherData.main.pressure)
+    const {lat, lon} = city.city.coord
+
 
     return (
       <tr>
-        <td>{name}</td>
-        <td>
-          <Sparklines height={90} width={180} data={temps}>
-            <SparklinesLine color={'red'} />
-          </Sparklines>
-        </td>
-        <td>
-          <Sparklines height={120} width={180} data={humidity}>
-            <SparklinesLine color={'blue'} />
-          </Sparklines>
-        </td>
-        <td>
-          <Sparklines height={120} width={180} data={pressure}>
-            <SparklinesLine color={'green'} />
-          </Sparklines>
-        </td>
+        <td><GMap lon={lon} lat={lat}></GMap></td>
+        <td><Chart data={temps} color={'orange'} units={'kelvin'} /></td>
+        <td><Chart data={humidities} color={'blue'} units={'aHa'} /></td>
+        <td><Chart data={pressures} color={'purple'} units={'bar'} /></td>
       </tr>
     )
   }
